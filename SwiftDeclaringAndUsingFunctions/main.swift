c911629c89810bba15a73c47201a3e05bf436ece
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-11 18:48:50 -0500

    functions

--- SwiftDeclaringAndUsingFunctions/main.swift
+++ SwiftDeclaringAndUsingFunctions/main.swift
@@ -4,10 +4,21 @@
 //
 //  Created by Baturay Kayaturk on 2019-02-11.
 //
-
 import Foundation
 
-print("Hello, World!")
-print("Baturay Kayaturk C0750127")
+
+func hello () {
+    print("Hello.")
+}
+
+hello()
+
+func sum(a : Int, b : Int) {
+    let c = a + b
+    print("Sum : " , c)
+}
+
+sum(a: 10, b: 30)
+
 
 
